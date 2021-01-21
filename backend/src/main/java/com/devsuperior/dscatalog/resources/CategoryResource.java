d4b3f7493a8e5f03447b8c51e8af37001070bbb5
Ulaecio <lenssonpol@hotmail.com>
2021-01-21 00:26:55 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,26 +1,28 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 //Recursos Rest
 @RestController //Realiza um pre-processamento antes do compilamento para o projeto funcionar corretamento
 @RequestMapping(value = "/categories") /*Rotas Rest do recurso que geralmente são colocados no plural*/
 public class CategoryResource {
 	
-//Criação de endpoints para(salvar, Deletar, buscar...)
+	@Autowired //Injetar a dependencia automática
+	private CategoryService service;
+	
+//Criação de endpoints CRUD (salvar, Deletar, buscar...)
 	@GetMapping //Para configurar que esse método a seguir vai ser um webservice
 	public ResponseEntity<List<Category>>	findAll() {
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Eletronics"));
+		List<Category> list = service.finAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
