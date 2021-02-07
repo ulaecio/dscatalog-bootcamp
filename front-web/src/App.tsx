52d6cc4851da476ca2c16978d0110904867295b8
ulaecio <lenssonpol@hotmail.com>
2021-02-07 00:45:47 -0300

    Add counter, setCouter with useState and useEffect

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,14 +1,30 @@
-import React from 'react';
-import Alert from './Alert';
+import React, {useState, useEffect} from 'react';
 
 const App = () => {
+    const [counter, setCounter] = useState(0);
+    
+    useEffect(() => {
+        console.log('componente iniciado')
+    }, []);
+
     return (
         <div className="container mt-5">
-            <Alert text= "Ulaecio"/>
-            <Alert text= "Alzenir"/>
-            <Alert text= "William"/>
-            <Alert />
-            <Alert />
+            <button 
+            className="btn btn-primary mr-5"
+            onClick={() => setCounter(counter + 1)}
+            >
+                +
+            </button>
+            <span>
+                {counter}
+            </span>
+            <button
+            className="btn btn-primary ml-5"
+            onClick={() => setCounter(counter - 1)}>
+                -
+            </button>
+            {counter > 5 && <h1>O valor é maior que 5</h1>}
+            {counter < 5 && <h1>O valor é menor que 5</h1>}
     </div>
     )
 }
