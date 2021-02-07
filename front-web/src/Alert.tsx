0376e86abb5761ce75b61cd11b81885bb3857c23
ulaecio <lenssonpol@hotmail.com>
2021-02-07 00:08:31 -0300

    Create front-web initial files

---
+++ front-web/src/Alert.tsx
@@ -0,0 +1,14 @@
+import { type } from 'os';
+import React from 'react';
+
+type Props = {
+    text?: string;
+}
+
+const Alert = ({ text }: Props) => (
+    <div className="alert alert-primary">
+        Hello {text}!
+    </div>
+)
+
+export default Alert;
---
+++ front-web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Alert from './Alert';
+
+const App = () => {
+    return (
+        <div className="container mt-5">
+            <Alert text= "Ulaecio"/>
+            <Alert text= "Alzenir"/>
+            <Alert text= "William"/>
+            <Alert />
+            <Alert />
+    </div>
+    )
+}
+
+export default App;
