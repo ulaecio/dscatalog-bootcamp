725b8bf3354bba8ecb854f99995ddfacab5e411b
ulaecio <lenssonpol@hotmail.com>
2021-02-08 17:51:23 -0300

    Add front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,31 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
     
-    useEffect(() => {
-        console.log('componente iniciado')
-    }, []);
-
     return (
-        <div className="container mt-5">
-            <button 
-            className="btn btn-primary mr-5"
-            onClick={() => setCounter(counter + 1)}
-            >
-                +
-            </button>
-            <span>
-                {counter}
-            </span>
-            <button
-            className="btn btn-primary ml-5"
-            onClick={() => setCounter(counter - 1)}>
-                -
-            </button>
-            {counter > 5 && <h1>O valor é maior que 5</h1>}
-            {counter < 5 && <h1>O valor é menor que 5</h1>}
-    </div>
+        <h1>App</h1>
     )
 }
 
