10fbac9a4f04ed4058092792fc36e6bde539fffb
ulaecio <lenssonpol@hotmail.com>
2021-02-09 17:30:38 -0300

    Add Routes Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './core/assets/styles/custom.scss';
 import './App.scss';
-import Navbar from './core/components/Navbar';
+import Routes from './Routes';
 
 const App = () => {
     return (
-        <Navbar />
-    )
+        <Routes />
+    );
 }
 
 export default App;
