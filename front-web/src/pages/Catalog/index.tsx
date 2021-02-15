aa67b81c582a813697f90ac6bd7e66666c9d8629
ulaecio <lenssonpol@hotmail.com>
2021-02-15 00:08:56 -0300

    Changing fech to Axios

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -4,12 +4,18 @@ import ProductCard from "./components/ProductCard";
 import "./styles.scss";
 
 const Catalog = () => {
-  //Quando o componente iniciar, buscar a lista de produto
-
   //Quando a lista de produtos estiver disponível,
   //popular um estado no componente e listar os produtos dianmicamente.
-
+  
+  //Quando o componente iniciar, buscar a lista de produto
   useEffect(() => {
+
+    //Limitações do fech: 
+    //muito verboso,
+    //não tem suporte pare ler progresso de upload de arquivos e
+    //não tem suporte nativo para enviar query de strings
+    //Assim substituiremos <fech> por <axios>
+    
     fetch('http://localhost:3000/products')
       .then((response) => response.json())
       .then((response) => console.log(response));
