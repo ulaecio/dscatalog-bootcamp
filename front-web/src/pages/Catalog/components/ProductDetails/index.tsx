46102a701aeb409ad4b77285fe187a8d39694f4a
ulaecio <lenssonpol@hotmail.com>
2021-02-14 22:12:27 -0300

    Create ProductDetails content

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { ReactComponent as ArrowIcon} from '../../../../core/assets/images/arrow.svg';
+import { ReactComponent as ProductImage} from '../../../../core/assets/images/product.svg';
 import './styles.scss';
 
 type ParamsType = {
@@ -18,6 +19,29 @@ const ProductDetails = () => {
                  < ArrowIcon className="icon-goback" />
                  <h1 className="text-goback">Voltar</h1>
                  </Link>
+                 <div className="row">
+                     <div className="col-6 pr-5"> 
+                         <div className="product-details-card text-center">
+                             <ProductImage className="product-detail-image" />
+                         </div>
+                         <h1 className="product-details-name">
+                             Computador Desktop - Intel Core i7
+                         </h1>
+                     </div>
+                     <div className="col-6 product-details-card"> 
+                         <h1 className="product-description-title">
+                             Descrição do produto
+                         </h1>
+                        <p className="product-description-title">
+                        Seja um mestre em multitarefas com a capacidade para exibir 
+                        quatro aplicativos simultâneos na tela. A tela está ficando 
+                        abarrotada? Crie áreas de trabalho virtuais para obter mais 
+                        espaço e trabalhar com os itens que você deseja. Além disso, 
+                        todas as notificações e principais configurações são reunidas 
+                        em uma única tela de fácil acesso.
+                        </p>
+                     </div>
+                 </div>
             </div>
         </div>
     );
