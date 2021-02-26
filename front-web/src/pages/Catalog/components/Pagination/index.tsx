c328f2fa2c8242698485cb21e271ca420bd26889
ulaecio <lenssonpol@hotmail.com>
2021-02-25 22:45:46 -0300

    Finish pagination components

--- front-web/src/pages/Catalog/components/Pagination/index.tsx
+++ front-web/src/pages/Catalog/components/Pagination/index.tsx
@@ -11,20 +11,28 @@ type Props = {
 
 const Pagination = ({ totalPages, activePage, onChange }: Props) => {
 const items = generateList( totalPages );
+const previousClass = totalPages > 0 && activePage > 0 ? 'page-active' : 'page-inactive';
+const nextClass = ( activePage +1 ) < totalPages ? 'page-active' : 'page-inactive';
 
   return (
     <div className="pagination-container">
-      <ArrowIcon className="pagination-previous" />
+      <ArrowIcon 
+        className={`pagination-previous ${previousClass}`}
+        onClick={() => onChange(activePage -1)}
+        />
       {items.map((item) => (
         <div 
          key={item}
          className={`pagination-item ${item === activePage ? 'active' : ''}`}
          onClick={() => onChange(item)}
          >
-          {item + 1}
+          {item }
         </div>
       ))}
-      <ArrowIcon className="pagination-next" />
+      <ArrowIcon 
+        className={`pagination-next ${nextClass}`}
+        onClick={() => onChange(activePage +1)}
+        />
     </div>
   );
 };
