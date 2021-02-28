272e55ed7e476a8b8c7875fa09fa0131ea34e46e
ulaecio <lenssonpol@hotmail.com>
2021-02-28 11:17:47 -0300

    Creating Admin navigation routes

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
-import NavBar from './components';
-
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./components";
+import "./styles.scss"
 const Admin = () => (
-  <div>
-      <NavBar />
+  <div className="admin-container">
+    <NavBar />
+    <div className="admin-content">
+      <Switch>
+        <Route path="/admin/products">
+          <h1>Products</h1>
+        </Route>
+        <Route path="/admin/categories">
+          <h1>Categories</h1>
+        </Route>
+        <Route path="/admin/users">
+          <h1>Users</h1>
+        </Route>
+      </Switch>
+    </div>
   </div>
 );
 
