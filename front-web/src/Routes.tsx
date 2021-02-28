9aaabe44c1978cb64e72d26c124ff167f19604af
ulaecio <lenssonpol@hotmail.com>
2021-02-28 12:23:24 -0300

    Fix routes from products

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,31 +1,30 @@
-import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Navbar from './core/components/Navbar';
-import Admin from './pages/Admin';
-import Catalog from './pages/Catalog';
-import ProductDetails from './pages/Catalog/components/ProductDetails';
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import Navbar from "./core/components/Navbar";
+import Admin from "./pages/Admin";
+import Catalog from "./pages/Catalog";
+import ProductDetails from "./pages/Catalog/components/ProductDetails";
+import Home from "./pages/Home";
 
 const Routes = () => (
-    <BrowserRouter>
+  <BrowserRouter>
     <Navbar />
     <Switch>
-        <Route path="/" exact>
-            <Home />
-        </Route>
-        <Route path="/products" exact>
-            <Catalog />
-        </Route>
-        <Route path="/products/:productId">
-            <ProductDetails />
-        </Route>
-        <Route path="/Admin">
-            <Redirect to="/admin/products"/>
-            <Admin />
-        </Route>
-    </Switch>    
-    </BrowserRouter>
-
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/products" exact>
+        <Catalog />
+      </Route>
+      <Route path="/products/:productId">
+        <ProductDetails />
+      </Route>
+      <Redirect from="/admin" to="/admin/products" exact/>
+      <Route path="/Admin">
+        <Admin />
+      </Route>
+    </Switch>
+  </BrowserRouter>
 );
 
 export default Routes;
