aea77c154b84f9cf4ec54fdd5103ae2726d490e8
ulaecio <lenssonpol@hotmail.com>
2021-04-10 00:03:12 -0300

    Error on SecuritConfig Resolvid

--- backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/AppConfig.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@Configuration
 public class AppConfig {
 	
 	@Bean
