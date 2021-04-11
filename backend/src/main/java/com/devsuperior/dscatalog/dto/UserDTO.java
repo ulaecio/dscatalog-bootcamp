bf96411243d2635d613381add0c5c5ee0b0072ba
ulaecio <lenssonpol@hotmail.com>
2021-04-11 18:18:10 -0300

    Basic Beans Validation

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -4,14 +4,22 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 import com.devsuperior.dscatalog.entities.User;
 
 public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Campo Obrigatório")
 	private String firstName;
 	private String lastName;
+	
+	@Email(message = "Favor entrar um email válido")
 	private String email;
 
 	
