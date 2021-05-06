8288f8abae8ef07bac125f3180ebfbffa6d34b5f
ulaecio <ulaecio@live.com>
2021-05-05 23:17:26 -0300

    Products Filters, COALESCE, list and Postgres Compatibility

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserDTO.java
@@ -6,7 +6,6 @@ import java.util.Set;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 
 import com.devsuperior.dscatalog.entities.User;
 
--- backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import javax.persistence.EntityNotFoundException;
@@ -32,8 +34,8 @@ public class ProductService {
 	
 	@Transactional (readOnly = true)
 	public Page<ProductDTO> findAllPaged(Long categoryId, String name, PageRequest pageRequest) {
-		Category category = (categoryId ==0) ? null : categoryRepository.getOne(categoryId);
-		Page<Product> list = repository.find(category, name, pageRequest);
+		List<Category> categories = (categoryId ==0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
+		Page<Product> list = repository.find(categories, name, pageRequest);
 		return list.map(x -> new ProductDTO(x));
 
 //pode ser utilizado desta foma abaixo também
