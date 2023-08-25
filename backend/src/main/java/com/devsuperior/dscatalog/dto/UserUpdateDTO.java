76a072c308b2c1b7b4870893f16133fd57fb0629
ulaecio <ulaecio@live.com>
2023-08-25 17:43:49 -0300

    Add dependencies S3 AWS and test upload file

--- backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/UserUpdateDTO.java
@@ -1,8 +1,8 @@
 package com.devsuperior.dscatalog.dto;
 
-import com.devsuperior.dscatalog.services.validation.UserInsertValid;
+import com.devsuperior.dscatalog.services.validation.UserUpdateValid;
 
-@UserInsertValid
+@UserUpdateValid
 public class UserUpdateDTO extends UserDTO{
 	private static final long serialVersionUID =1L;
 
