6505594255e0e9035c4c5b880b59a47cdb789961
ulaecio <ulaecio@live.com>
2023-08-26 12:50:07 -0300

    S3 Upload endpoint

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.dto.UriDTO;
 import com.devsuperior.dscatalog.services.ProductService;
 
 //Recursos Rest
@@ -61,6 +63,12 @@ public class ProductResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(dto.getId()).toUri();
 		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PostMapping(value = "/image")
+	public ResponseEntity<UriDTO> uploadImage(@RequestParam MultipartFile file) {
+		UriDTO dto = service.uploadFile(file);
+		return ResponseEntity.ok().body(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
--- backend/src/main/java/com/devsuperior/dscatalog/services/S3Service.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/S3Service.java
@@ -1,17 +1,20 @@
 package com.devsuperior.dscatalog.services;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
+import org.apache.commons.io.FilenameUtils;
+import org.joda.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Service
 public class S3Service {
@@ -24,19 +27,31 @@ public class S3Service {
 	@Value("${s3.bucket}")
 	private String bucketName;
 
-	public void uploadFile(String localFilePath) {
+	public URL uploadFile(MultipartFile file) {
 		try {
-			File file = new File(localFilePath);
-			LOG.info("Upload start");
-			s3client.putObject(new PutObjectRequest(bucketName, "test.jpg", file));
-			LOG.info("Upload end");
+			//Pega o nome original do arquivo e a extensão do arquivo.
+			String originalName = file.getOriginalFilename();
+			String extension = FilenameUtils.getExtension(originalName);
+			//Pega o instante do tempo atual e concatena com o a extensão do arquivo(Ex: jpg, png, pdf...)
+			String fileName = Instant.now().toDate().getTime() + "." + extension;
+			//Envia o arquivo para o S3 com seu contentType(Ex: jpg, png, pdf...)
+			InputStream is = file.getInputStream();
+			String contentType = file.getContentType();
+			//Função que chama o apload do arquivo e gera a URL do arquivo.
+			return uploadFile(is, fileName, contentType);
 		}
-		catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException: " + e.getErrorMessage());
-			LOG.info("Status code: " + e.getErrorCode());
+		catch (IOException e) {
+			throw new IllegalArgumentException(e.getMessage());
 		}
-		catch (AmazonClientException e) {
-			LOG.info("AmazonClientException: " +  e.getMessage());
-		}
+	}
+
+	private URL uploadFile(InputStream is, String fileName, String contentType) {
+		ObjectMetadata meta = new ObjectMetadata();
+		meta.setContentType(contentType);
+		LOG.info("Upload start");
+		s3client.putObject(bucketName, fileName, is, meta);
+		LOG.info("Upload finished");
+		//Retorna a url do bucket
+		return s3client.getUrl(bucketName, fileName);
 	}
 }
